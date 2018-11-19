f1a9c18eda77d1429c89ce81aada73ff9cff6724
Ryan Aday <ryanobelisk@gmail.com>
2018-11-18 23:45:10 -0500

    Added app.py, made changes to devlog

---
+++ app.py
@@ -0,0 +1,17 @@
+#Team RanQuoR-Turkey
+#P01 - ArRESTed Development oh dear
+#Period 7
+#2018-11-18
+
+
+import urllib.request, json
+from flask import Flask, render_template
+from util import parse_API
+
+app = Flask(__name__)
+
+@app.route('/')
+
+if (__name__) == "__main__"
+    app.debug = True
+    app.run()
