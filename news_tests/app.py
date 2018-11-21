1b78894e7a3aefe03cb96f813c32f8f83bed47b2
cmohri <clara.mohri@gmail.com>
2018-11-20 22:33:10 -0500

    Update weather API

--- news_tests/app.py
+++ news_tests/app.py
@@ -11,20 +11,31 @@ URL = URL_STUB + KEY
 
 @app.route("/", methods=["GET"])
 def index():
-    global data
+    global data, l
     req = urllib.request.urlopen(URL)
-    data = json.loads(req.read())
-    return render_template("index.html", data=data, section = 'sport')
+    data = json.loads(req.read())    
+    l = data['response']['results']
+    s = set()
+    #get the different sections, but no repeats. Thus using a set
+    for i in l:
+       s.add(i['sectionName'])
+    return render_template("index.html", data=data, section = s)
 
 @app.route("/category", methods = ["POST"])
 def get_category():
-    global data
+    global data, l
     if request.method == "POST":
-        if request.form.get('category') == 'politics':        
+        for i in l:
+            print(i['sectionName'])
+            if i['sectionName'] == request.form.get('category'):
+                print('found')
+                print(i['webTitle'], i['sectionName'])
+        return request.form.get('category')
+        if request.form.get('category') == 'Society':        
             return render_template("index.html", data = data, section = 'SPORT')
         if request.form.get('category') == None:
             return redirect(url_for("index"))
-
+    return("hello")
 
 if __name__ == "__main__":
     app.debug = True
